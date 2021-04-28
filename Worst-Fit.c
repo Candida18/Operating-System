2cbb763e825aab8600985a0afaa7b734518baa80
Candida Noronha <64965389+Candida18@users.noreply.github.com>
2021-04-28 21:24:28 +0530

    Create Worst-Fit.c

---
+++ Worst-Fit.c
@@ -0,0 +1,79 @@
+#include<stdio.h>
+int main()
+{
+	int fragments[10], blocks[10], files[10];
+	int m, n, number_of_blocks, number_of_files, temp, top = 0;
+	static int block_arr[10], file_arr[10];
+
+	printf("\n------------------------------------------------------------------------------------------------\n");
+    printf("\n \t\t\t\tMemory Management Scheme - Worst-Fit\n");
+    printf("\n------------------------------------------------------------------------------------------------\n");
+
+	printf("\n Enter the Total Number of Blocks : ");
+	scanf("%d",&number_of_blocks);
+	printf("\n------------------------------------------------------------------------------------------------\n");
+
+	printf("\n Enter the Total Number of Process : ");
+	scanf("%d",&number_of_files);
+	printf("\n------------------------------------------------------------------------------------------------\n");
+
+	printf("\n Enter the Size of the Blocks : \n");
+
+	for(m = 0; m < number_of_blocks; m++)
+	{
+		printf(" Block No. [%d] : ", m + 1);
+		scanf("%d", &blocks[m]);
+	}
+	printf("\n------------------------------------------------------------------------------------------------\n");
+
+	printf("\n Enter the Size of the Process : \n");
+	for(m = 0; m < number_of_files; m++)
+	{
+		printf(" Process No. [%d] : ", m + 1);
+		scanf("%d", &files[m]);
+	}
+	printf("\n------------------------------------------------------------------------------------------------\n");
+
+	for(m = 0; m < number_of_files; m++)
+	{
+		for(n = 0; n < number_of_blocks; n++)
+		{
+			if(block_arr[n] != 1)
+			{
+				temp = blocks[n] - files[m];
+				if(temp >= 0)
+				{
+					if(top < temp)
+					{
+						file_arr[m] = n;
+						top = temp;
+					}
+				}
+			}
+			fragments[m] = top;
+			block_arr[file_arr[m]] = 1;
+
+		}
+		top = 0;
+	}
+	
+	printf("\n Process_no.\tProcess Size\tBlock Number\tBlock Size\tFragment\n");
+    for(m = 0; m < number_of_files; m++)
+    {
+        printf("\n  %d\t\t%d\t\t%d\t\t%d\t\t%d\n\n", m+1, files[m], file_arr[m]+1,blocks[file_arr[m]],fragments[m]);
+    }
+   
+	printf("\n------------------------------------------------------------------------------------------------\n");
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
